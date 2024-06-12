0f6e6ad10e30a826f38783b026cba861fbea2cf9
Grzegorz Zielinski <gregoryblack11@gmail.com>
2024-06-12 20:51:28 +0200

    fix: deleted unused const

--- tests/api/articles/articles-head.spec.ts
+++ tests/api/articles/articles-head.spec.ts
@@ -3,8 +3,7 @@ import { APIResponse, expect, test } from '@playwright/test';
 import { Endpoints } from '@_src_api/enums/api-endpoints.enum';
 
 test.describe('HEAD/articles endpoint tests', async () => {
-    const articles: string = '/api/articles';
-
+    
   test('Returns 200 OK for all articles', async ({ request }) => {
     // When
     const response: APIResponse = await request.head(Endpoints.ARTICLES);
